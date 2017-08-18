bf3397cc65dfc202315bb07f42533554f84c7960
Federico Maria Ceruti <federicomaria.ceruti@mail.polimi.it>
2017-08-18 15:37:52 +0200

    cmmit

--- Cpp/AppOCL/main.cpp
+++ Cpp/AppOCL/main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
 #include <CL/cl.h>
 
+
 int main()
 {
 	std::cout << "Hello, World!" << std::endl;
+
+
+	cl_device_id device_id = NULL;
+	cl_context context = NULL;
+	cl_command_queue command_queue = NULL;
+	cl_mem memobj = NULL;
+	cl_program program = NULL;
+	cl_kernel kernel = NULL;
+	cl_platform_id platform_id = NULL;
+	cl_uint ret_num_devices;
+	cl_uint ret_num_platforms;
+	cl_int ret;
+
+
+	/* Get Platform and Device Info */
+	ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
+
+
+	std::cout << ret_num_platforms << std::endl;
+
+
 	return 0;
 }
